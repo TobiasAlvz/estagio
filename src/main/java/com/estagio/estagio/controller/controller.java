5006ec7db99a3215c9953387d9304e4b99c39fc3
Tobias Alves <tobiasjose12@gmail.com>
2024-03-15 10:14:16 -0300

    tasks.java

--- src/main/java/com/estagio/estagio/controller/controller.java
+++ src/main/java/com/estagio/estagio/controller/controller.java
@@ -1,5 +1,8 @@
 package com.estagio.estagio.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,21 +15,21 @@ import org.springframework.web.bind.annotation.RequestBody;
 @Controller
 public class controller {
 
-    @GetMapping("/tasks")
-    public String tasks() {
-        return "tasks";
+    List<Task> tasks = new ArrayList<>();
 
+    @GetMapping("/tasks")
+    public String tasksPage() {
+        return "tasks";
     }
 
     @PostMapping("/tasks")
-    public void tasks(Task tasks) {
-
-        System.out.println("Tarefa:" + tasks.getDescription());
+    public void addTask(@RequestBody Task task) {
+        long id = tasks.size() + 1L;
+        tasks.add(new Task(id, task.getDescription(), task.getPriority()));
     }
 
     @GetMapping("/register")
-    public String register() {
+    public String registerPage() {
         return "register";
-
     }
 }
