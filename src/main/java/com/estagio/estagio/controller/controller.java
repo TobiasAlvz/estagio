bb99e79ffdf4377afed876139b384ffe00a35c4a
Tobias Alves <tobiasjose12@gmail.com>
2024-03-15 09:53:19 -0300

    tasks

--- src/main/java/com/estagio/estagio/controller/controller.java
+++ src/main/java/com/estagio/estagio/controller/controller.java
@@ -2,6 +2,12 @@ package com.estagio.estagio.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.estagio.estagio.model.Task;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class controller {
@@ -10,6 +16,12 @@ public class controller {
     public String tasks() {
         return "tasks";
 
+    }
+
+    @PostMapping("/tasks")
+    public void tasks(Task tasks) {
+
+        System.out.println("Tarefa:" + tasks.getDescription());
     }
 
     @GetMapping("/register")
